caef208c9be06f0acb6340fcc4f1dac305021612
Claudia Naveda! <claudialaboral@gmail.com>
2023-02-05 16:26:05 -0300

    Prueba1

---
+++ js/unEvento.js
@@ -0,0 +1,6 @@
+
+var boton=document.getElementById("miboton");
+boton.addEventListener("click",function(){
+  var capa=document.getElementsById("micapa");
+capa.innerHTML="Has pulsado un boton";
+},false);
